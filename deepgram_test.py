cbac734065b9a32383c3aa847ecf428faf63adc6
Sprilt21 <nkpqiu@gmail.com>
2023-06-22 17:03:39 -0400

    Update deepgram_test.py
    attempting two api calls with async

--- deepgram_test.py
+++ deepgram_test.py
@@ -3,24 +3,26 @@
 from deepgram import Deepgram
 import asyncio
 import json
-from config import *
+import copy
+#from config import *
 
 # Location of the file you want to transcribe. Should include filename and extension.
 # Example of a local file: ../../Audio/life-moves-pretty-fast.wav
 # Example of a remote file: https://static.deepgram.com/examples/interview_speech-analytics.wav
-FILE = 'audio/e.m4a'
+FILE = 'audio/ec.m4a'
 
 # Mimetype for the file you want to transcribe
 # Include this line only if transcribing a local file
 # Example: audio/wav
 MIMETYPE = 'audio/mp4'
 
-
+FIRST_API_KEY = '426ede75de63902dcec1758f2825fdecf4144b73'
+SECOND_API_KEY = 'c76cd20140827b65299eace0dc4fb9ffa17dcce1'
 async def main():
 
     # Initialize the Deepgram SDK
-    deepgram = Deepgram(DEEPGRAM_API_KEY)
-
+    deepgram = Deepgram(FIRST_API_KEY)
+    deepgram2 = Deepgram(SECOND_API_KEY)
     # Check whether requested file is local or remote, and prepare source
     if FILE.startswith('http'):
         # file is remote
@@ -28,6 +30,7 @@ async def main():
         source = {
             'url': FILE
         }
+
     else:
         # file is local
         # Open the audio file
@@ -53,7 +56,25 @@ async def main():
 
     # Write the response to the console
     print(json.dumps(response, indent=4))
+    
+    audio = open(FILE, 'rb')
+    source = {
+            'buffer': audio,
+            'mimetype': MIMETYPE
+    }
+    response2 = await asyncio.create_task(
+        deepgram.transcription.prerecorded(
+            source,
+            {
+                'punctuate': True,
+                'model': 'base',
+                'language': 'en'
+            }
+        )
+    )
 
+    # Write the response to the console
+    print(json.dumps(response2, indent=4))
     # Write only the transcript to the console
     # print(response["results"]["channels"][0]["alternatives"][0]["transcript"])
 
